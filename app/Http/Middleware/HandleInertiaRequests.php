58de58e40527819fccdc19e1cd48c781e253585c
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2025-10-18 09:20:14 +0330

    pass 'auth' as inertia shared prop

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Middleware;
 
 class HandleInertiaRequests extends Middleware
@@ -36,15 +36,10 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
-        [$message, $author] = str(Inspiring::quotes()->random())->explode('-');
 
         return [
             ...parent::share($request),
-            'name' => config('app.name'),
-            'quote' => ['message' => trim($message), 'author' => trim($author)],
-            'auth' => [
-                'user' => $request->user(),
-            ],
+            'auth' => Auth::user() ? Auth::user()->toResource() : null,
         ];
     }
 }
--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,21 +1,7 @@
-export interface Auth {
-    user: User;
-}
-
-export interface SharedData {
-    name: string;
-    quote: { message: string; author: string };
-    auth: Auth;
-    [key: string]: unknown;
-}
-
 export interface User {
-    id: number;
-    name: string;
-    email: string;
-    avatar?: string;
-    email_verified_at: string | null;
-    created_at: string;
-    updated_at: string;
-    [key: string]: unknown; // This allows for additional properties...
+	id: number;
+	name: string;
+	email: string;
+	created_at: string;
+	updated_at: string;
 }
