81ae1166a3a0161c8d99963a91996010520c1b68
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2025-10-18 09:36:35 +0330

    install biomejs and cleanup resources/ and routes/ files

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,20 +1,22 @@
-import '../css/app.css';
+import "../css/app.css";
 
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createRoot } from 'react-dom/client';
-
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+import { createInertiaApp } from "@inertiajs/react";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { createRoot } from "react-dom/client";
 
 createInertiaApp({
-    title: (title) => title ? `${title} - ${appName}` : appName,
-    resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
-    setup({ el, App, props }) {
-        const root = createRoot(el);
+  resolve: (name) =>
+    resolvePageComponent(
+      `./pages/${name}.tsx`,
+      import.meta.glob("./pages/**/*.tsx"),
+    ),
+  setup({ el, App, props }) {
+    const root = createRoot(el);
 
-        root.render(<App {...props} />);
-    },
-    progress: {
-        color: '#4B5563',
-    },
+    root.render(<App {...props} />);
+  },
+  progress: {
+    color: "red",
+    showSpinner: true,
+  },
 });
