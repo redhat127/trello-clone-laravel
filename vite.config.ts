8e227da667352a92991c1eea760ac1f14692fa29
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2025-11-01 08:57:49 +0330

    remove 'resources/css/app.css' from vite laravel plugin

--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,19 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            ssr: 'resources/js/ssr.tsx',
-            refresh: true,
-        }),
-        react(),
-        tailwindcss(),
-        wayfinder({
-            formVariants: true,
-        }),
-    ],
-    esbuild: {
-        jsx: 'automatic',
-    },
+  plugins: [
+    laravel({
+      input: ['resources/js/app.tsx'],
+      ssr: 'resources/js/ssr.tsx',
+      refresh: true,
+    }),
+    react(),
+    tailwindcss(),
+    wayfinder({
+      formVariants: true,
+    }),
+  ],
+  esbuild: {
+    jsx: 'automatic',
+  },
 });
