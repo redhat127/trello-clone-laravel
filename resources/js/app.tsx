8d3f15bec1aee6f3290d943e489f2a91e667901e
saeid ebrahimi <saeid.ebrahimi127@gmail.com>
2025-11-01 08:46:32 +0330

    cleanup resources/ and routes/

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,10 +4,7 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-
 createInertiaApp({
-    title: (title) => title ? `${title} - ${appName}` : appName,
     resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
@@ -15,6 +12,7 @@ createInertiaApp({
         root.render(<App {...props} />);
     },
     progress: {
-        color: '#4B5563',
+        color: 'red',
+        showSpinner: true,
     },
 });
